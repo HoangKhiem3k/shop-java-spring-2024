764a7787eeab26c199f8b8469026b315d183fe97
hoangkhiem3k <ledohoangkhiem3k@gmail.com>
2024-11-12 23:51:02 +0700

    spring initializr

---
+++ src/main/java/com/project/shop_spring_2024/ShopSpring2024Application.java
@@ -0,0 +1,13 @@
+package com.project.shop_spring_2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopSpring2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopSpring2024Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/shop_spring_2024/ShopSpring2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.shop_spring_2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopSpring2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
