0bed6d7f2c57a460fa9dc59f66bc46f18c7e9da7
hoangkhiem3k <ledohoangkhiem3k@gmail.com>
2024-12-02 23:26:25 +0700

    authen-autho

---
+++ src/main/java/com/project/shop_spring_2024/components/JwtTokenUtil.java
@@ -0,0 +1,79 @@
+package com.project.shop_spring_2024.components;
+
+import com.project.shop_spring_2024.exceptions.InvalidParamException;
+import com.project.shop_spring_2024.models.User;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Value;
+import io.jsonwebtoken.Claims;
+
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+@Component
+@RequiredArgsConstructor
+public class JwtTokenUtil {
+    @Value("${jwt.expiration}")
+    private int expiration; //save to an environment variable
+    @Value("${jwt.secretKey}")
+    private String secretKey;
+
+    private Key getSignInKey(){
+        byte[] bytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(bytes);
+    }
+
+    public String generateToken(User user) throws Exception{
+        Map<String,Object> claims = new HashMap<>();
+        claims.put("phoneNumber", user.getPhoneNumber());
+        try {
+            String token = Jwts.builder()
+                    .setClaims(claims)
+                    .setSubject(user.getPhoneNumber())
+                    .setExpiration(new Date(System.currentTimeMillis() + expiration * 1000L))
+                    .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                    .compact();
+            return token;
+        }catch (Exception e){
+            throw  new InvalidParamException("Cannot create jwt token, error: "+e.getMessage());
+        }
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public  <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = this.extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    public String extractPhoneNumber(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public boolean isTokenExpired(String token) {
+        Date expirationDate = this.extractClaim(token, Claims::getExpiration);
+        return expirationDate.before(new Date());
+    }
+
+    public boolean validateToken(String token, UserDetails userDetails) {
+        String phoneNumber = extractPhoneNumber(token);
+        return (phoneNumber.equals(userDetails.getUsername()))
+                && !isTokenExpired(token);
+    }
+}
+
